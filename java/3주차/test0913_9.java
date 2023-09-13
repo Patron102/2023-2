a2a2d866e2919c6eced646f72332ffd38477eaf7
빠뜨론 <129668361+Patron102@users.noreply.github.com>
2023-09-13 16:14:52 +0900

    Update and rename test0913_9.java to java/3주차/test0913_9.java

---
+++ java/3주차/test0913_9.java
@@ -0,0 +1,39 @@
+package week3;
+
+public class test0913_9 {
+
+	public static void main(String[] args) {
+
+//		- 문제 1
+//		String title = "로미오와 줄리엣";
+//		int author = "윌리엄 셰익스피어";
+//		int price = 5.94;
+//		System.out.printf("제목 : %s\n", title);
+//		System.out.printf("저자 : %s\n", author);
+//		System.out.printf("가격 : %f\n", price);
+
+//		- 정답
+//		String title = "로미오와 줄리엣";
+//		String author = "윌리엄 셰익스피어";
+//		double price = 5.94;
+//		System.out.printf("제목 : %s\n", title);
+//		System.out.printf("저자 : %s\n", author);
+//		System.out.printf("가격 : %.2f\n", price);
+		
+		
+//		- 문제 2
+//		double tall = 176.6;
+//		double weight = 73.34;
+//		System.out.printf("신장 : %.dcm\n", tall);
+//		System.out.printf("체중 : %.dkg\n", weight);
+		
+//		- 정답
+//		double tall = 176.6;
+//		double weight = 73.34;
+//		System.out.printf("신장 : %.1fcm\n", tall);
+//		System.out.printf("체중 : %.2fkg\n", weight);
+		
+		
+	}
+
+}
