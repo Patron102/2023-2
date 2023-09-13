97d51af395cbbe4ff522487ac5583d2d337afb97
빠뜨론 <129668361+Patron102@users.noreply.github.com>
2023-09-13 16:12:42 +0900

    Delete java/2주차 directory

--- java/2주차/My_Introduction.java
+++
@@ -1,9 +0,0 @@
-package introduction;
-
-public class My_Introduction {
-
-	public static void main(String[] args) {
-		System.out.println("제 이름은 박효기입니다" + System.lineSeparator() + "저는 개쩌는 사람입니다" + System.lineSeparator() + "감사니다");
-	}
-
-}
